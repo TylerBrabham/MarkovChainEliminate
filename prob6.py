4ecbc7643baf3282abd1c10a3d8c07024f7cf0da
Tyler Brabham <tylerbrabham@hotmail.com>
2014-02-03 19:37:13 -0800

    finished problem 6

--- prob6.py
+++ prob6.py
@@ -17,11 +17,11 @@ for a and a value for b.
 '''
 
 def eliminate(query_node,evidence_node,initial_dist,transition,query_val,evidence_val):
-	# Initialize. place p(x_i | x_pi_i) on active list
+	#Pick ordering. Do not include first node. Handle first node as special case.
 	ordering = range(2,evidence_node+1)
 	ordering.reverse()
 
-	#put in initial factors
+	#Initialize. Put in initial factors
 	active_list = []
 	for state in range(2):
 		factor = ((1,state),None,initial_dist[state])
@@ -35,15 +35,15 @@ def eliminate(query_node,evidence_node,initial_dist,transition,query_val,evidenc
 				active_list.append(factor)
 
 	#Evidence. Last node only, just a single number
-	if evidence_node == 1:
-		delta = ((evidence_node, 1),None,1.0)
+	if evidence_val==1:
+		delta = ((evidence_node,1),None,1.0)
 		active_list.append(delta)
 		delta = ((evidence_node, 0),None,0.0)
 		active_list.append(delta)
 	else:
-		delta = ((evidence_node, 1),None,1.0)
+		delta = ((evidence_node, 1),None,0.0)
 		active_list.append(delta)
-		delta = ((evidence_node, 0),None,0.0)
+		delta = ((evidence_node, 0),None,1.0)
 		active_list.append(delta)
 
 	#Update
@@ -60,11 +60,13 @@ def eliminate(query_node,evidence_node,initial_dist,transition,query_val,evidenc
 		active_list = new_active
 
 		#multiply them all together, sum over the value of x_i (0 or 1)
-		factors = [[(i-1,0),None,0], [(i-1,1),None,0]]
+		factors = [[(i-1,0),None,0], [(i-1,1),None,0]] #new factors
 		for j in [0,1]:
+			#sum over previous nodes value
 			temp_sum = 0
 
 			for k in [0,1]:
+				#sum over current nodes value
 				temp_prod = 1.0
 
 				for elm in i_list:
@@ -73,7 +75,6 @@ def eliminate(query_node,evidence_node,initial_dist,transition,query_val,evidenc
 						temp_prod *= elm[2]
 
 				temp_sum += temp_prod
-
 			factors[j][2] = temp_sum
 
 		#place new factor on active list.
@@ -95,7 +96,9 @@ def eliminate(query_node,evidence_node,initial_dist,transition,query_val,evidenc
 
 	return numerator/denominator
 
+# Values for problem 6
 transition = {(0,0):.6, (0,1):.4, (1,0):.2, (1,1):.8}
 initial = {0:.5, 1:.5}
 
+# Query for problem 6
 print eliminate(1,4,initial,transition,0,1)
