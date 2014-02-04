d6874e0d968edbce6e63439419c6f46bfddb3553
Tyler Brabham <tylerbrabham@hotmail.com>
2014-02-03 19:30:33 -0800

    changes

--- prob6.py
+++ prob6.py
@@ -1,5 +1,5 @@
 # Tyler Brabham
-# CSC281A Spring 2014
+# CS C281A Spring 2014
 # HW1 Prob6
 
 '''
@@ -16,41 +16,38 @@ Input to function is a matrix of transition probabilities, a value
 for a and a value for b.
 '''
 
-def eliminate(last_node,evidence,transition):
+def eliminate(query_node,evidence_node,initial_dist,transition,query_val,evidence_val):
 	# Initialize. place p(x_i | x_pi_i) on active list
-	ordering = range(1,last_node+1)
+	ordering = range(2,evidence_node+1)
 	ordering.reverse()
 
+	#put in initial factors
 	active_list = []
-	for i in ordering:
-		if i==1:
-			factor = ((i,0),None,.5)
-			active_list.append(factor)
-			factor = ((i,1),None,.5)
-			active_list.append(factor)
-		else:
-			factor = ((i,0),(i-1,0),.6)
-			active_list.append(factor)
-			factor = ((i,1),(i-1,0),.4)
-			active_list.append(factor)
-			factor = ((i,0),(i-1,1),.2)
-			active_list.append(factor)
-			factor = ((i,1),(i-1,1),.8)
-			active_list.append(factor)
-	print active_list
+	for state in range(2):
+		factor = ((1,state),None,initial_dist[state])
+		active_list.append(factor)
+
+	#put in transition factors
+	for i in range(2, evidence_node+1):
+		for state in range(2):
+			for prev_state in range(2):
+				factor = ((i,state), (i-1,prev_state), transition[(prev_state,state)])
+				active_list.append(factor)
+
 	#Evidence. Last node only, just a single number
-	if evidence == 1:
-		delta = ((last_node, 1),None,1)
+	if evidence_node == 1:
+		delta = ((evidence_node, 1),None,1.0)
 		active_list.append(delta)
-		delta = ((last_node, 0),None,0)
+		delta = ((evidence_node, 0),None,0.0)
 		active_list.append(delta)
 	else:
-		delta = ((last_node, 1),None,0)
+		delta = ((evidence_node, 1),None,1.0)
 		active_list.append(delta)
-		delta = ((last_node, 0),None,1)
+		delta = ((evidence_node, 0),None,0.0)
 		active_list.append(delta)
 
 	#Update
+	numerator_list = []
 	for i in ordering:
 		# find all potentitals with i in them
 		i_list = []
@@ -66,28 +63,39 @@ def eliminate(last_node,evidence,transition):
 		factors = [[(i-1,0),None,0], [(i-1,1),None,0]]
 		for j in [0,1]:
 			temp_sum = 0
+
 			for k in [0,1]:
 				temp_prod = 1.0
+
 				for elm in i_list:
-					if elm[1]==None:
-						if elm[0][1]==k:
-							temp_prod *= elm[2]
-						else:
-							pass
-					else:
-						if elm[0][1]==k and elm[1][1]==j:
-							temp_prod *= elm[2]
-						else:
-							pass
+					#multiply all the terms together
+					if elm[0][1]==k and (elm[1]==None or elm[1][1]==j):
+						temp_prod *= elm[2]
+
 				temp_sum += temp_prod
+
 			factors[j][2] = temp_sum
-			print temp_sum
 
 		#place new factor on active list.
-		active_list.extend(factors)
+		active_list.extend(factors) 
 
-		print active_list
-	
 	#normalize
+	denominator = 0.0
+	numerator = 0.0
+	for j in range(2):
+		temp_prod = 1.0
 
-eliminate(4,1,None)
+		for elm in active_list:
+			if elm[0][1]==j:
+				temp_prod *= elm[2]
+
+		denominator += temp_prod
+		if j==query_val:
+			numerator += temp_prod
+
+	return numerator/denominator
+
+transition = {(0,0):.6, (0,1):.4, (1,0):.2, (1,1):.8}
+initial = {0:.5, 1:.5}
+
+print eliminate(1,4,initial,transition,0,1)
